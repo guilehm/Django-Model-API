904877a3bd8c76a63eeb12331938aab5323d082c
guile.hm <guile.hm@hotmail.com>
2018-04-11 15:01:49 -0300

    Requests url

--- client/views.py
+++ client/views.py
@@ -8,7 +8,8 @@ def index(request):
 
 
 def products(request):
-    products = requests.get('http://localhost:8000/api/products').json()
+    # products = requests.get('http://localhost:8000/api/products').json()
+    products = requests.get('https://gui-api.herokuapp.com/api/products').json()
     context = {
         'products' : products,
     }
@@ -17,6 +18,7 @@ def products(request):
 
 def product(request, product_slug):
     product = requests.get('http://localhost:8000/api/products/' + product_slug).json()
+    product = requests.get('https://gui-api.herokuapp.com/api/products/' + product_slug).json()
     print(product)
     context = {
         'product' : product,
