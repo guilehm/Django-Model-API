76412c749c727fb83bc942da68cecade6920afcf
guile.hm <guile.hm@hotmail.com>
2018-04-22 16:59:55 -0300

    heroku requests

--- client/views.py
+++ client/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import requests
-import json
 
 # Create your views here.
 def index(request):
@@ -8,8 +7,8 @@ def index(request):
 
 
 def products(request):
-    products = requests.get('http://localhost:8000/api/products').json()
-    # products = requests.get('https://gui-api.herokuapp.com/api/products').json()
+    # products = requests.get('http://localhost:8000/api/products').json()
+    products = requests.get('https://gui-api.herokuapp.com/api/products').json()
     context = {
         'products' : products,
     }
@@ -17,8 +16,8 @@ def products(request):
 
 
 def product(request, product_slug):
-    product = requests.get('http://localhost:8000/api/products/' + product_slug).json()
-    # product = requests.get('https://gui-api.herokuapp.com/api/products/' + product_slug).json()
+    # product = requests.get('http://localhost:8000/api/products/' + product_slug).json()
+    product = requests.get('https://gui-api.herokuapp.com/api/products/' + product_slug).json()
     print(product)
     context = {
         'product' : product,
@@ -26,8 +25,8 @@ def product(request, product_slug):
     return render(request,'client/product.html', context)
 
 def select(request):
-    products = requests.get('http://localhost:8000/api/products').json()
-    # products = requests.get('https://gui-api.herokuapp.com/api/products').json()
+    # products = requests.get('http://localhost:8000/api/products').json()
+    products = requests.get('https://gui-api.herokuapp.com/api/products').json()
     context = {
         'products' : products,
     }
